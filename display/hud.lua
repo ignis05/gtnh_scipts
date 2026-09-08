12b5fdd060dcaf56f1192b287488e94e5f94e42e
ignis05 <48099798+ignis05@users.noreply.github.com>
2026-09-09 01:01:34 +0200

    shape done

--- display/hud.lua
+++ display/hud.lua
@@ -11,11 +11,11 @@ local config = {
     fontSize = 1,
 }
 
-local function asRGB(color)
-    local r = math.floor(color / 65536) % 256
-    local g = math.floor(color / 256) % 256
-    local b = color % 256
-    return r, g, b
+local function RGB(hex)
+  local r = ((hex >> 16) & 0xFF) / 255.0
+  local g = ((hex >> 8) & 0xFF) / 255.0
+  local b = ((hex) & 0xFF) / 255.0
+  return r, g, b
 end
 
 local function newQuad(glasses, p1, p2, p3, p4, color)
@@ -24,11 +24,8 @@ local function newQuad(glasses, p1, p2, p3, p4, color)
     quad.setVertex(2, p2[1], p2[2])
     quad.setVertex(3, p3[1], p3[2])
     quad.setVertex(4, p4[1], p4[2])
-
-    if quad.setColor then
-        local r, g, b = asRGB(color)
-        quad.setColor(r, g, b)
-    end
+    quad.setColor(RGB(color))
+    quad.setAlpha(0.9)
 
     return quad
 end
@@ -40,7 +37,7 @@ local function newText(glasses, text, x, y, scale, color)
     label.setScale(scale or 1)
 
     if label.setColor then
-        local r, g, b = asRGB(color)
+        local r, g, b = RGB(color)
         label.setColor(r, g, b)
     end
 
@@ -77,11 +74,13 @@ local function setupGlass(glasses)
     newQuad(glasses, {0, y - b1}, {3.5 * h + l + b2 + 1, y - b1}, {2.5 * h + l + 1, y - b1 - h - b2}, {0, y - b1 - h - b2}, borderColor)
     -- bottom bar
     newQuad(glasses, {0, y}, {3.5 * h + l + b2 + 1, y}, {3.5 * h + l + b2 + 1, y - b1}, {0, y - b1}, borderColor)
-    -- blue bar
-    newQuad(glasses, {3.5 * h, y - b1}, {3.5 * h + l, y - b1}, {2.5 * h + l, y - b1 - h}, {2.5 * h, y - b1 - h}, panelColor)
-
     local ui = {}
-    ui.energyBar = newQuad(glasses, {b2 + 3.25 * h, y - b1}, {b2 + 3.25 * h, y - b1}, {b2 + 2.25 * h, y - b1 - h}, {b2 + 2.25 * h, y - b1 - h}, accentColor)
+    ui.energyBar = newQuad(glasses,
+        {3.5 * h, y - b1},
+        {3.5 * h + l, y - b1},
+        {2.5 * h + l, y - b1 - h},
+        {2.5 * h, y - b1 - h},
+        panelColor)
     ui.textPercent = newText(glasses, "0.0%", b2 + 2.1 * h, y - b1 - h / 1.8 - config.fontSize, config.fontSize, accentColor)
     ui.textCurr = newText(glasses, "", b2 + 3.25 * h + 1, y - b1 - h / 2 - config.fontSize, config.fontSize / 1.3, textColor)
     ui.textMax = newText(glasses, "", 2.25 * h + l - 1.5 * config.fontSize, y - b1 - h / 2 - config.fontSize, config.fontSize / 1.3, textColor)
@@ -89,12 +88,16 @@ local function setupGlass(glasses)
     return ui
 end
 
-local function updateBar(quad, xStart, xEnd, yBase, height, percent)
-    local endY = yBase - height * percent
-    quad.setVertex(1, xStart, yBase)
-    quad.setVertex(2, xEnd, yBase)
-    quad.setVertex(3, xEnd, endY)
-    quad.setVertex(4, xStart, endY)
+local function updateBar(quad, yBase, height, percent)
+    local leftBottomX = 3.5 * config.height
+    local leftTopX = 2.5 * config.height
+    local rightBottomX = leftBottomX + config.length * percent
+    local rightTopX = leftTopX + config.length * percent
+
+    quad.setVertex(1, leftBottomX, yBase)
+    quad.setVertex(2, rightBottomX, yBase)
+    quad.setVertex(3, rightTopX, yBase - height)
+    quad.setVertex(4, leftTopX, yBase - height)
 end
 
 local function main()
@@ -122,10 +125,8 @@ local function main()
             for _, entry in ipairs(glassesList) do
                 local ui = entry.ui
                 local y = config.resolution[2] / config.GUIscale
-                local xStart = config.borderTop + 3.25 * config.height
-                local xEnd = xStart + config.length * percentage
 
-                updateBar(ui.energyBar, xStart, xEnd, y - config.borderBottom, config.height, percentage)
+                updateBar(ui.energyBar, y - config.borderBottom, config.height, percentage)
 
                 ui.textPercent.setText(string.format("%.1f%%", percentage * 100))
                 ui.textCurr.setText(formatNumber(currentEnergy) .. " EU")
